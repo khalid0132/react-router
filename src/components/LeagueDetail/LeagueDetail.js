f1756614167a99911f00563e8c0b295d69039c31
Khalid Saifullah <khalid0132@gmail.com>
2021-03-10 20:54:52 +0100

    fetching, viewing, responsive, icons everying is done

--- src/components/LeagueDetail/LeagueDetail.js
+++ src/components/LeagueDetail/LeagueDetail.js
@@ -24,9 +24,9 @@ const LeagueDetail = () => {
             console.log(data.leagues[0]);
         })
     },[idLeague])
-    const {strSport, strLeague, dateFirstEvent, strCountry, strGender, strBanner} = leagueDetail;
+    const {strSport, strLeague, dateFirstEvent, strCountry, strGender, strBadge} = leagueDetail;
     
-    //IMAGE SHOWS ON PAGE DEPENDING ON MALE OR FEMALE
+    //IMAGE SHOWS ON PAGE DEPENDING ON MALE OR FEMALE BY USING CONDITIONAL RENDERING
     let leagueImage = leagueDetail.strGender;
     const imgUrl = leagueImage === 'Male' ? maleImg : femaleImg;
     // let imgUrl;
@@ -39,30 +39,29 @@ const LeagueDetail = () => {
     
     return (
        <div className="bg-dark">
-            <Container>
-            <Row>
-                <Col><img className="image-responsive" src={strBanner} alt=""/></Col>
-            </Row>
-        </Container>
+           <h1 className ="top-header"><img className="image-responsive" src={strBadge} alt=""/></h1>
         <Container >
-            <Row className="justify-content-md-center mt-2 text-left rounded bg-success text-white">
-                <Col md="8" className="w-25 league-detail">
+        {/* <Row>
+                <Col md='12'><img className="image-responsive top-header" src={strBadge} alt=""/></Col>
+            </Row> */}
+            <Row className="justify-content-md-center mt-2 mr-4 text-left rounded bg-success text-white">
+                <Col md="8" className=" pt-3 league-detail">
                         <h4>{strLeague} </h4>
                         <p><FontAwesomeIcon icon={faMapMarker}/> Founded: {dateFirstEvent}</p>
                         <p><FontAwesomeIcon icon={faFlag}/> Country: {strCountry}</p>
                         <p><FontAwesomeIcon icon={faFutbol}/> Sport Type: {strSport}</p>
                         <p><FontAwesomeIcon icon={faMars}/> Gender: {strGender}</p>
                 </Col>
-                <Col md="4" className="pt-4 league-detail">
-                    <p><img style={{width: '300px'}} src={imgUrl} alt=""/></p> 
+                <Col md="4" className="pt-3 league-detail">
+                    <p><img className="league-detail" style={{width: '300px'}} src={imgUrl} alt=""/></p> 
                 </Col>
             </Row>
                 <Row className="mt-5">
-                    <Col className="ml-5 mr-5 text-left text-white">
+                    <Col className="text-left text-white">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed hic sit quidem. Quisquam illo, molestiae pariatur quod impedit magnam dolores ut eveniet, illum distinctio totam quidem numquam! Pariatur voluptates repellat temporibus recusandae unde doloribus perspiciatis expedita corrupti at, neque debitis rerum laudantium quasi consequuntur esse, sunt aperiam reiciendis ipsam in. Repellat, beatae. Mollitia itaque illum tempora voluptatum earum totam voluptate optio iusto ab alias omnis esse debitis nesciunt recusandae, voluptatem corrupti adipisci laboriosam possimus incidunt perferendis distinctio! Cupiditate consectetur tempore aut, voluptatem rerum dolores nemo quo numquam similique ipsum error commodi ex nesciunt inventore dolore rem vel! Tempora, autem consequuntur?
                     </Col>
                 </Row>
-                <Row className="pt-5">
+                <Row className="pt-5 pb-1">
                     <Col>
                         
                        <a href="https://www.facebook.com/groups/completewebdevelopment3/?multi_permalinks=495133901506539"> <span><FontAwesomeIcon icon={faFacebook}/></span></a>
